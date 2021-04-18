4c7e6d245a62ff2242342679b5c0a1e3f7a48f71
MateuszRoslak <222554@edu.p.lodz.pl>
2021-04-18 13:33:06 +0200

    Modified Order class

--- src/main/java/edu/iis/mto/time/Order.java
+++ src/main/java/edu/iis/mto/time/Order.java
@@ -1,41 +1,41 @@
 package edu.iis.mto.time;
 
+import java.time.Clock;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Order {
-
-    private static final long VALID_PERIOD_HOURS = 24;
+    protected static final long VALID_HOURS = 24;
+    private final List<OrderItem> items = new ArrayList<>();
+    private final Clock startClock;
     private State orderState;
-    private List<OrderItem> items = new ArrayList<>();
-    private LocalDateTime subbmitionDate;
+    private LocalDateTime localDateTime;
 
     public Order() {
         orderState = State.CREATED;
+        this.startClock = Clock.systemDefaultZone();
     }
+
+    public Order(Clock clock) {
+        orderState = State.CREATED;
+        this.startClock = clock;
+    }
+
 
     public void addItem(OrderItem item) {
         requireState(State.CREATED, State.SUBMITTED);
-
-        items.add(item);
-        orderState = State.CREATED;
-
-    }
-
-    public void submit() {
         requireState(State.CREATED);
 
         orderState = State.SUBMITTED;
-        subbmitionDate = LocalDateTime.now();
-
+        localDateTime = LocalDateTime.now(startClock);
     }
 
     public void confirm() {
         requireState(State.SUBMITTED);
-        long hoursElapsedAfterSubmittion = subbmitionDate.until(LocalDateTime.now(), ChronoUnit.HOURS);
-        if (hoursElapsedAfterSubmittion > VALID_PERIOD_HOURS) {
+        long elapsedTime = localDateTime.until(LocalDateTime.now(startClock), ChronoUnit.HOURS);
+        if (elapsedTime > VALID_HOURS) {
             orderState = State.CANCELLED;
             throw new OrderExpiredException();
         }
@@ -57,12 +57,10 @@ public class Order {
                 return;
             }
         }
-
         throw new OrderStateException("order should be in state "
-                                      + allowedStates
-                                      + " to perform required  operation, but is in "
-                                      + orderState);
-
+                + allowedStates
+                + " to perform required  operation, but is in "
+                + orderState);
     }
 
     public enum State {
